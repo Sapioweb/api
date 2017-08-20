10c6290d6ed9c006ae9a161e06e15561eeccd6b7
Andreas Beasley <andreas@sapioweb.com>
2017-08-19 23:36:38 -0400

    delete and grab company website

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var morgan = require('morgan');
 var mongoose = require('mongoose');
 var request = require('request');
 var cheerio = require('cheerio');
+var axios = require('axios');
 var config = require('./config');
 var Blog = require('./app/models/blog');
 var Newsletter = require('./app/models/newsletter');
@@ -23,6 +24,7 @@ app.use(morgan('dev'));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE, PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
@@ -95,6 +97,36 @@ app.get('/companies', function (req, res) {
   });
 });
 
+app.delete('/companies', function (req, res) {
+  Company.findByIdAndRemove(req.query.company, (err, companies) => {
+    if (err) throw err;
+
+    res.send({ companies });
+  });
+});
+
+app.get('/companies/website', function (req, res) {
+  Company.findOne({_id: req.query.company}, (err, company) => {
+    axios.get(company.url).then((response) => {
+      var $ = cheerio.load(response.data);
+
+      $('.ListingDetails_Level1_HEADERBOX').filter((i, elm) => {
+        company.link = $(elm).find('.ListingDetails_Level1_SITELINK').attr('href');
+
+        Company.findOneAndUpdate({
+          _id: company._id
+        }, company, {
+          upsert: true
+        },function(err) {
+          if (err) throw err;
+        });
+
+        res.send({company});
+      });
+    });
+  });
+});
+
 app.get('/fetch/blog', function (req, res) {
   request({
     url: 'https://api.github.com/users/sapiobeasley/gists',
@@ -125,90 +157,145 @@ app.get('/fetch/blog', function (req, res) {
 });
 
 app.get('/scrape', function (req, res) {
-  request({
-    url: req.query.link,
-    headers: {
-      'User-Agent': 'agentOrange'
+  axios.get(req.query.link).then( (response) => {
+    var $ = cheerio.load(response.data);
+
+    var companyList = [];
+
+    if ($('.ListingResults_All_CONTAINER').length > 0) {
+      $('.ListingResults_All_CONTAINER').each((i, elm) => {
+
+        if ($(elm).find('.level2_footer_left_box_a').length == 0) {
+          console.log({
+            company: $(elm).find('*[itemprop = "name"]').text(),
+          });
+        }
+
+        companyData = {
+          url: 'http://web.tampachamber.com' + $(elm).find('.level1_footer_left_box_a').attr('href') || $(elm).find('.level2_footer_left_box_a').attr('href'),
+          company: $(elm).find('*[itemprop = "name"]').text(),
+          phone: $(elm).find('.ListingResults_Level1_PHONE1').text() || $(elm).find('.ListingResults_Level1_PHONE2').text() || $(elm).find('.ListingResults_Level2_PHONE1').text(),
+          address: $(elm).find('*[itemprop = "street-address"]').text(),
+          locality: $(elm).find('*[itemprop = "locality"]').text(),
+          region: $(elm).find('*[itemprop = "region"]').text(),
+          zip: $(elm).find('*[itemprop = "postal-code"]').text(),
+        };
+
+        companyList.push(companyData);
+
+        Company.findOneAndUpdate({
+          company: $(elm).find('*[itemprop = "name"]').text()
+        }, companyData, {
+          upsert: true
+        },function(err) {
+          if (err) throw err;
+        });
+      });
+    } else {
+      $('.ListingDetails_Level1_HEADER').filter((i, elm) => {
+        companyData = {
+          url: 'http://web.tampachamber.com/no-url',
+          company: $(elm).find('*[itemprop = "name"]').text(),
+          phone: $(elm).find('.ListingResults_Level1_PHONE1').text() || $(elm).find('.ListingResults_Level1_PHONE2').text() || $(elm).find('.ListingResults_Level2_PHONE1').text() || $(elm).find('.ListingDetails_Level1_MAINCONTACT').text(),
+          address: $(elm).find('*[itemprop = "street-address"]').text(),
+          locality: $(elm).find('*[itemprop = "locality"]').text(),
+          region: $(elm).find('*[itemprop = "region"]').text(),
+          zip: $(elm).find('*[itemprop = "postal-code"]').text(),
+        };
+
+        companyList.push(companyData);
+
+        Company.findOneAndUpdate({
+          company: $(elm).find('*[itemprop = "name"]').text()
+        }, companyData, {
+          upsert: true
+        },function(err) {
+          if (err) throw err;
+        });
+      });
+
+      $('.ListingDetails_Level2_HEADER').filter((i, elm) => {
+        companyData = {
+          url: 'http://web.tampachamber.com/no-url',
+          company: $(elm).find('*[itemprop = "name"]').text(),
+          phone: $(elm).find('.ListingResults_Level1_PHONE1').text() || $(elm).find('.ListingResults_Level1_PHONE2').text() || $(elm).find('.ListingResults_Level2_PHONE1').text() || $(elm).find('.ListingDetails_Level1_MAINCONTACT').text() || $(elm).find('.ListingDetails_Level2_MAINCONTACT').text(),
+          address: $(elm).find('*[itemprop = "street-address"]').text(),
+          locality: $(elm).find('*[itemprop = "locality"]').text(),
+          region: $(elm).find('*[itemprop = "region"]').text(),
+          zip: $(elm).find('*[itemprop = "postal-code"]').text(),
+        };
+
+        companyList.push(companyData);
+
+        Company.findOneAndUpdate({
+          company: $(elm).find('*[itemprop = "name"]').text()
+        }, companyData, {
+          upsert: true
+        },function(err) {
+          if (err) throw err;
+        });
+      });
     }
-  }, (error, response, body) => {
-    if (error) throw error;
 
-    var $ = cheerio.load(body);
-
-    var company, address, locality, region, zip, phone, link;
-
-    var json = { company : '', address : '', locality : '', region : '', zip : '', phone : '', link: ''};
-
-    $('*[itemprop = "name"]').filter(function(){
-      var data = $(this);
-
-      json.company = data.text();
-    });
-
-    $('*[itemprop = "street-address"]').filter(function(){
-      var data = $(this);
-
-      json.address = data.text();
-    });
-
-    $('*[itemprop = "locality"]').filter(function(){
-      var data = $(this);
-
-      json.locality = data.text();
-    });
-
-    $('*[itemprop = "region"]').filter(function(){
-      var data = $(this);
-
-      json.region = data.text();
-    });
-
-    $('*[itemprop = "postal-code"]').filter(function(){
-      var data = $(this);
-
-      json.zip = data.text();
-    });
-
-    $('.ListingDetails_Level2_MAINCONTACT').filter(function(){
-      var data = $(this);
-
-      json.phone = data.text().replace(/\s/g,'');
-    });
-
-    $('.ListingDetails_Level1_MAINCONTACT').filter(function(){
-      var data = $(this);
-
-      json.phone = data.text().replace(/\s/g,'');
-    });
-
-    $('*[itemprop = "url"]').filter(function(){
-      var data = $(this);
-
-      json.link = data.children().last().attr('href');
-    });
-
-    Company.findOneAndUpdate({
-      company: json.company
-    }, {
-      company: json.company,
-      address: json.address,
-      locality: json.locality,
-      region: json.region,
-      zip: json.zip,
-      phone: json.phone,
-      link: json.link
-    }, {
-      upsert: true
-    },function(err) {
-      if (err) throw err;
-
-      console.log('Company saved successfully');
-    });
-
-    res.json({ url: req.query.link, company: json });
+    return companyList;
+  }).then((companyList) => {
+    res.json(companyList);
   });
+});
 
+app.get('/scrape/website', function (req, res) {
+  console.log(req.params);
+  // Company.find({
+  //   link: null,
+  //   url: {
+  //     $exists : true
+  //   }
+  // }, (err, companies) => {
+  //   if (err) throw err;
+  //
+  //   for (var i = 0; i < companies.length; i++) {
+  //     axios.get(companies[i].url).then((response) => {
+  //
+  //       console.log($('.ListingDetails_Level2_HEADER').length);
+  //
+  //       $('.ListingDetails_Level1_HEADER').filter((i, elm) => {
+  //
+  //         console.log($(elm).find('.ListingDetails_Level1_SITELINK').length);
+  //
+  //         companyData = {
+  //           link: $(elm).find('.ListingDetails_Level1_SITELINK').attr('href')
+  //         };
+  //
+  //         console.log(companyData);
+  //       //       companyList.push(companyData);
+  //       //
+  //       //       Company.findOneAndUpdate({
+  //       //         company: $(elm).find('*[itemprop = "name"]').text()
+  //       //       }, companyData, {
+  //       //         upsert: true
+  //       //       },function(err) {
+  //       //         if (err) throw err;
+  //       //       });
+  //       });
+  //     });
+    // }
 
+    // res.send({ companies });
+  // });
+  // axios.get(req.query.link).then( (response) => {
+  //   var $ = cheerio.load(response.data);
+  //
+  //   var companyList = [];
+  //
+  //   if ($('.ListingResults_All_CONTAINER').length > 0) {
+  //   } else {
+
+  //   }
+  //
+  //   return companyList;
+  // }).then((companyList) => {
+  //   res.json(companyList);
+  // });
 });
 
 app.listen(3001);
