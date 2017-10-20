eb1b059953667390f3b3e719125364e5fe0f3681
Andreas Beasley <andreas@sapioweb.com>
2017-10-19 23:38:22 -0400

    inquire endpoint

--- app/routes/email.js
+++ app/routes/email.js
@@ -9,7 +9,21 @@ router.post('/send/:email', function (req, res) {
   var data = {
     from: 'Andreas Beasley <andreas@sapioweb.com>',
     to: req.params.email,
-    bcc: config.mailgun.bcc,
+    subject: req.body.subject,
+    text: req.body.text
+  };
+
+  mailgun.messages().send(data, function (error, body) {
+    console.log(body);
+  });
+
+  res.send(req.query);
+});
+
+router.post('/inquire', function (req, res) {
+  var data = {
+    from: 'sapioweb.com <info@sapioweb.com>',
+    to: 'info@sapioweb.com',
     subject: req.body.subject,
     text: req.body.text
   };
