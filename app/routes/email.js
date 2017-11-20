a0adbb0c2e3f1186399d0bd06e1cdca7e39f7b8c
Andreas Beasley <andreas@sapioweb.com>
2017-11-19 22:06:36 -0500

    marketing update

--- app/routes/email.js
+++ app/routes/email.js
@@ -20,6 +20,21 @@ router.post('/send/:email', function (req, res) {
   res.send(req.query);
 });
 
+router.post('/roi', function (req, res) {
+  var data = {
+    from: 'Sapioweb ROI <info@sapioweb.com>',
+    to: 'info@sapioweb.com',
+    subject: req.body.subject,
+    text: req.body.text
+  };
+
+  mailgun.messages().send(data, function (error, body) {
+    console.log(body);
+  });
+
+  res.send(req.query);
+});
+
 router.post('/inquire', function (req, res) {
   var data = {
     from: 'sapioweb.com <info@sapioweb.com>',
